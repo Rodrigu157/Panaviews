6489c674bda7242826c6eaee6be4de9c3a34c908
Carlos Rodriguez <carlos.rodriguez18rr@gmail.com>
2024-11-05 21:50:46 -0500

    fix: direccion del archivo de categorias php,  faltas ortograficas en el footer

--- js/sitios.js
+++ js/sitios.js
@@ -1,6 +1,6 @@
 let totalImages = 0
 function obtenerCategorias() {
-    fetch('../PhP/categoria.php')
+    fetch('https://panaviews.vercel.app/PhP/categoria.php')
         .then(response => response.json())
         .then(data => {
             const categoriasContainer = document.getElementById('carousel');
