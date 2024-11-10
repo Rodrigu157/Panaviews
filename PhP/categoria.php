33c5714406b2fa0608efea16e57a8cd2f2c4c352
Carlos Rodriguez <carlos.rodriguez18rr@gmail.com>
2024-11-10 12:53:53 -0500

    Prueba de archivo conexion

--- PhP/categoria.php
+++ PhP/categoria.php
@@ -1,6 +1,7 @@
 <?php
 include 'conexion.php';  // Incluir archivo de conexión
 
+/*
 $query = "SELECT * FROM categoria";  // Consulta para obtener todos los registros
 $resultado = $conexion->query($query);
 
@@ -12,4 +13,15 @@ if ($resultado->num_rows > 0) {
 }
 
 echo json_encode($categorias);  // Convertir el array a JSON
+*/
+
+try {
+    // Realizar una consulta para obtener datos de una tabla
+    $stmt = $pdo->query("SELECT * FROM categoria");
+    // Devolver los datos como JSON
+    echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
--- PhP/conexion.php
+++ PhP/conexion.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $host = "bdb7sbx26cvnng2lbiv9-mysql.services.clever-cloud.com";// Nombre del host
 $usuario = "ucyebez9xuaqeic8";              // Nombre de usuario
 $contraseña = "qsng4TiRbXu0uB0vqVq5";       // Contraseña
@@ -11,6 +12,15 @@ $conexion = new mysqli($host, $usuario, $contraseña, $base_datos);
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
+
+*/
+// Configuración de conexión
+$host = 'bdb7sbx26cvnng2lbiv9-mysql.services.clever-cloud.com';
+$dbname = 'bdb7sbx26cvnng2lbiv9';
+$username = 'ucyebez9xuaqeic8';
+$password = 'qsng4TiRbXu0uB0vqVq5';
+// Crear una nueva conexión PDO
+$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 
 
 ?>
